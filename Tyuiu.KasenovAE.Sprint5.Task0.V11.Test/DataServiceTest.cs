f1212231ea98fc6bdc083b8ef4585c303b6ba0f0
AidarhanKasenov <Tyuiu.KasenovAE@outlook.com>
2023-11-19 14:58:33 +0500

    Add project files.

---
+++ Tyuiu.KasenovAE.Sprint5.Task0.V11.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.KasenovAE.Sprint5.Task0.V11.Lib;
+
+namespace Tyuiu.KasenovAE.Sprint5.Task0.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\Users\kasen\source\repos\Tyuiu.KasenovAE.Sprint5\Tyuiu.KasenovAE.Sprint5.Task0.V11\bin\Debug\OutPutFileTask0.txt";
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileExists = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+        [TestMethod]
+        public void CheckedResult()
+        {
+            string path = @"C:\Users\kasen\source\repos\Tyuiu.KasenovAE.Sprint5\Tyuiu.KasenovAE.Sprint5.Task0.V11\bin\Debug\OutPutFileTask0.txt";
+            string fileRes = File.ReadAllText(path);
+            string res = "-2,556";
+            Assert.AreEqual(res, fileRes);
+        }
+    }
+}
---
+++ Tyuiu.KasenovAE.Sprint5.Task0.V11/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KasenovAE.Sprint5.Task0.V11.Lib;
+
+namespace Tyuiu.KasenovAE.Sprint5.Task0.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнил: Касенов А. Е. | ПКТб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнил: Касенов Айдархан Есенаевич | ПКТб-23-2                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение вычислить его значение при x = 3,                        *");
+            Console.WriteLine("* результат сохранить в текстовый файл OutPutFileTask0.txt и вывести      *");
+            Console.WriteLine("* на консоль. Округлить до трёх знаков после запятой.                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int x = 3;
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            DataService ds = new DataService();
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
